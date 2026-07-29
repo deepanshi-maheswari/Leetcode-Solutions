73cafda7507693784b0b22b78308a02ae26a4600
deepanshi-maheswari <deepanshimaheswari27@gmail.com>
2026-07-29 20:41:39 +0530

    Time: 0 ms (100%), Space: 20.1 MB (47.4%) - LeetHub

--- 0203-remove-linked-list-elements/0203-remove-linked-list-elements.cpp
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.cpp
@@ -12,26 +12,55 @@ class Solution {
 public:
     ListNode* removeElements(ListNode* head, int val) {
         
-        ListNode dummy(0);
-        dummy.next = head;
+        // ListNode dummy(0);
+        // dummy.next = head;
 
-        ListNode* prev = &dummy;
+        // ListNode* prev = &dummy;
+        // ListNode* curr = head;
+
+        // while (curr != NULL) {
+
+        //     if (curr->val == val) {
+        //         prev->next = curr->next;
+        //         ListNode* temp = curr;
+        //         curr = curr->next;
+        //         delete temp;
+        //     }
+        //     else {
+        //         prev = curr;
+        //         curr = curr->next;
+        //     }
+        // }
+
+        // return dummy.next;
+
+        while(head != NULL && head->val == val){
+            ListNode* temp = head;
+            head = head->next;
+            temp->next = NULL;
+            delete temp;
+        }
+
+        if(head == NULL)
+            return NULL;
+
+
+        //if multiple same val present then:
         ListNode* curr = head;
-
-        while (curr != NULL) {
-
-            if (curr->val == val) {
-                prev->next = curr->next;
-                ListNode* temp = curr;
-                curr = curr->next;
-                delete temp;
+        
+        while(curr->next != NULL){
+            if(curr->next->val == val){
+                ListNode* temp = curr->next;
+                curr->next = curr->next->next;
+                //temp->next = NULL;
+                delete temp; 
             }
-            else {
-                prev = curr;
+            else{
                 curr = curr->next;
             }
         }
 
-        return dummy.next;
+
+        return head;
     }
 };
