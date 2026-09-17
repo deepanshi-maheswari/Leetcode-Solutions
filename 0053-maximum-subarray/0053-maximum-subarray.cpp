d15ec3a6eb0b5f6c34487d128cc76ee4cf3404c4
deepanshi-maheswari <deepanshimaheswari27@gmail.com>
2026-09-17 23:04:41 +0530

    Time: 0 ms (100%), Space: 71.9 MB (14.88%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,46 +1,19 @@
 class Solution {
 public:
-   int maxSubArrayHelper(vector<int>& nums, int start, int end){
-    if(start == end) return nums[start];
-
-    int maxLeftBoaderSum = INT_MIN, maxRightBoaderSum = INT_MIN;
-    int leftBoaderSum = 0, rightBoaderSum = 0;
-
-    int mid = start + (end - start) / 2;
-
-    int maxLeftSum = maxSubArrayHelper(nums, start, mid);
-    int maxRightSum = maxSubArrayHelper(nums, mid + 1, end);
-
-    // Max Cross Boader Sum.
-    for(int i = mid; i >= start; i--){
-        leftBoaderSum += nums[i];
-        if(leftBoaderSum > maxLeftBoaderSum){
-            maxLeftBoaderSum = leftBoaderSum;
-        }
-    }
-
-    for(int i = mid + 1; i <= end; i++){
-        rightBoaderSum += nums[i];
-        if(rightBoaderSum > maxRightBoaderSum){
-            maxRightBoaderSum = rightBoaderSum;
-        }
-    }
-    int crossBoaderSum = maxLeftBoaderSum + maxRightBoaderSum;
-
-    return max(maxLeftSum, max(maxRightSum, crossBoaderSum));
-   } 
-    
     int maxSubArray(vector<int>& nums) {
-        // int sum = nums[0];
-        // int maxi = nums[0];
+        int n = nums.size();
 
-        // for(int i = 1; i < nums.size(); i++){
-        //     sum = max(nums[i] , sum + nums[i]);
-        //     maxi = max(sum , maxi);
-        // }
+        int sum = 0;
+        int ans = nums[0];
+        for(int j = 0; j < n; j++){
+            sum += nums[j];
+            ans = max(ans, sum);
 
-        // return maxi;
+            if(sum < 0){
+                sum = 0;
+            }
+        }
 
-        return maxSubArrayHelper(nums, 0, nums.size() - 1);
+        return ans;
     }
 };
